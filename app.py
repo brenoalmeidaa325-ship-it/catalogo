bbe8d9513ba8d3dfece094df0b267edfb8a96b07
brenoalmeidaa325-ship-it <brenoalmeidaa325@gmail.com>
2026-08-05 14:03:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "ROTA OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
