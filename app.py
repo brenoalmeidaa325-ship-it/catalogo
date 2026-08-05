3319a96972677a706dbc01dea8bf8b51b8f73f70
brenoalmeidaa325-ship-it <brenoalmeidaa325@gmail.com>
2026-08-05 13:54:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return str(os.listdir("."))
 
 if __name__ == "__main__":
     app.run()
